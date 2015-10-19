0a8d11121b6e33857d24d6d53942c66e4fd914ff
Yuliana Apaza <yuliana.apaza94@gmail.com>
2015-10-19 06:08:59 -0500

    update register

--- src/clientepando/Main.java
+++ src/clientepando/Main.java
@@ -10,14 +10,26 @@ public class Main
         Cliente c = new Cliente();
         Scanner scan = new Scanner(System.in);
         String linea = "";
-
+        int indice;
+        
         while(true)
         {
             System.out.print("pandosql> ");
             linea = scan.nextLine();
+            
             if(linea.equals("exit"))
                 break;
-            c.enviar(linea);
+            
+            if(linea.contains(";") && (indice = linea.indexOf(";")) != linea.length()){
+                do {
+                    indice++;
+                    c.enviar(linea.substring(0, indice));
+                    linea = linea.substring(indice);
+                    indice = linea.indexOf(";");
+                } while(indice != -1 && indice+1 != linea.length());
+            }
+            
+            //c.enviar(linea);
         }
         c.desconectar();
     }
